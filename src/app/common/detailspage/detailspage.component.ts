6d507c4a0b20b88e8a1b98c18ed2312d6382483c
beymedhabib <beymedhabib@gmail.com>
2020-11-05 12:50:46 +0100

    up

--- src/app/common/detailspage/detailspage.component.ts
+++ src/app/common/detailspage/detailspage.component.ts
@@ -10,7 +10,6 @@ import { TodoService } from 'src/app/service/todo.service';
 export class DetailspageComponent implements OnInit {
   constructor(private Activatedroute: ActivatedRoute, private todoservice: TodoService) { }
   id = this.Activatedroute.snapshot.paramMap.get('i');
-  societe = JSON.parse(localStorage.getItem('societes')) || [];
   data;
   ngOnInit(): void {
     this.getone();
